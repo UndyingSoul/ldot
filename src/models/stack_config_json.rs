e1ff18c83589195d4bfa821190af0bdbe0360ec7
Corbin Whitton <undyingsoul.mc@gmail.com>
2023-09-26 02:59:32 -0600

    renamed stack config model

--- src/models/stack_config_json.rs
+++ src/models/stack_config_json.rs
@@ -2,41 +2,48 @@ use serde::{Deserialize, Deserializer};
 use serde_json::Value;
 
 #[derive(Deserialize)]
-struct StackConfig {
+pub struct StackConfig {
     #[serde(default)] // Make version field optional and default to an empty string
-    version: String,
-    stack_name: String,
+    pub version: String,
+    pub stack_name: String,
     #[serde(default)] // Make description field optional and default to an empty string
-    description: String,
-    projects: Vec<ProjectConfig>,
+    pub description: String,
+    pub projects: Vec<ProjectConfig>,
+    pub scripts: Vec<ScriptConfig>,
 }
 
 #[derive(Deserialize)]
-struct ProjectConfig {
-    project_name: String,
+pub struct ProjectConfig {
+    pub project_name: String,
     #[serde(default)] // Make project_description field optional and default to an empty string
-    project_description: String,
-    stages: Vec<StageConfig>,
+    pub project_description: String,
+    pub stages: Vec<StageConfig>,
 }
 
 #[derive(Deserialize)]
-struct StageConfig {
-    stage_name: String,
+pub struct StageConfig {
+    pub stage_name: String,
     #[serde(default)] // Make stage_description field optional and default to an empty string
-    stage_description: String,
+    pub stage_description: String,
     #[serde(default)] // Make prerequisites field optional and default to an empty array
-    prerequisites: Vec<String>,
+    pub prerequisites: Vec<String>,
     #[serde(default)] // Make commands field optional and default to an empty array
-    commands: Vec<String>,
+    pub commands: Vec<String>,
 }
-
+#[derive(Deserialize)]
+pub struct ScriptConfig {
+    pub script_name: String,
+    #[serde(default)] // Make project_description field optional and default to an empty string
+    pub script_description: String,
+    pub commands: Vec<String>,
+}
 // Deserialize JSON string into StackConfig
-fn deserialize_stack_config(json_str: &str) -> Result<StackConfig, serde_json::Error> {
+pub fn deserialize_stack_config(json_str: &str) -> Result<StackConfig, serde_json::Error> {
     serde_json::from_str(json_str)
 }
 
 // Deserialize JSON string into StackConfig using custom deserialization logic
-fn deserialize_stack_config_custom(json_str: &str) -> Result<StackConfig, serde_json::Error> {
+pub fn deserialize_stack_config_custom(json_str: &str) -> Result<StackConfig, serde_json::Error> {
     let mut config: StackConfig = serde_json::from_str(json_str)?;
 
     // Handle the optional fields with default values
@@ -68,3 +75,4 @@ fn deserialize_stack_config_custom(json_str: &str) -> Result<StackConfig, serde_
 
     Ok(config)
 }
+
